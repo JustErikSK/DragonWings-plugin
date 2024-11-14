c4f351901cff06d51e6621612f7d284b970a5509
justerik <sebo.erik@gmail.com>
2024-11-14 12:58:54 +0100

    Dragon drops elytra on death - coded, might need to get player's position and drop it there / put it inside the player's inventory

--- src/main/java/me/minecraft/plugin/dragonDropsPlugin/DragonDropsPlugin.java
+++ src/main/java/me/minecraft/plugin/dragonDropsPlugin/DragonDropsPlugin.java
@@ -1,11 +1,30 @@
 package me.minecraft.plugin.dragonDropsPlugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DragonDropsPlugin extends JavaPlugin {
+public final class DragonDropsPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "DragonDrops >> Plugin has been enabled!");
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void dragonDefeat(EntityDeathEvent e) {
+        LivingEntity ent = e.getEntity();
+
+        if (ent.getType() == EntityType.ENDER_DRAGON) {
+            e.getDrops().add(new ItemStack(Material.ELYTRA));
+        }
     }
 }

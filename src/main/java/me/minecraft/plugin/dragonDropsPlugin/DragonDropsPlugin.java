9428cfc8df0767983d3901ef5e7cbf0e36d716ab
justerik <sebo.erik@gmail.com>
2024-11-14 10:49:39 +0100

    Initial commit

---
+++ src/main/java/me/minecraft/plugin/dragonDropsPlugin/DragonDropsPlugin.java
@@ -0,0 +1,11 @@
+package me.minecraft.plugin.dragonDropsPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DragonDropsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
